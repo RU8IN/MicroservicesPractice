6cf5d596bbed2724af016cc6fc7995f1f4c4b46f
infinity <iskatel.03@mail.ru>
2024-06-09 15:51:03 +0300

    Добавлена поддержка FeignClients в customer

--- customer/src/main/java/com/ru8in/customer/CustomerApplication.java
+++ customer/src/main/java/com/ru8in/customer/CustomerApplication.java
@@ -3,9 +3,13 @@ package com.ru8in.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(
+        basePackages = "com.ru8in.clients"
+)
 public class CustomerApplication {
 
     public static void main(String[] args) {
